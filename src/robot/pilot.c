8380eb2e01dc75d9709f808620883505626a8b03
AntoineLBT <antoine.lebout@gmail.com>
2018-02-04 21:42:04 +0100

    initializing of piloc.c

--- src/robot/pilot.c
+++ src/robot/pilot.c
@@ -1,6 +1,15 @@
-/* -*- mode: c; c-basic-offset: 4; indent-tabs-mode: nil -*- */  
+/* -*- mode: c; c-basic-offset: 4; indent-tabs-mode: nil -*- */
 // a completer
-   
-       
-   
+#include <stdlib.h>
+#include <stdio.h>
+#include "pilot.h"
 
+
+void Pilot_free();
+void Pilot_start();
+void Pilot_new();
+void Pilot_stop();
+void Pilot_setVelocity();
+void Pilot_check();
+void Pilot_checkMAE();
+PilotState Pilot_getState();
